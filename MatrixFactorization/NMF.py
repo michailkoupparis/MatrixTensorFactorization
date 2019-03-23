bfd961c50a68a0601649e354fb40d0040c18d61b
michailkoupparis <koupparis95@gmail.com>
2019-03-23 18:12:46 +0000

    Adding init graphs

--- MatrixFactorization/NMF.py
+++ MatrixFactorization/NMF.py
@@ -1,56 +1,53 @@
 import numpy as np
 import math
 from numpy.random import RandomState
-
-
+from sklearn.decomposition import nmf as NMF_sk
+EPSILON = np.finfo(np.float32).eps
 
 
 distribution_beta = {'gaussian':2, 'poisson':1, 'gamma':0}
 
 
+def gaussian_phi(V,W,H,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H):
+    #var = (V.std() ** 2) 
+    var = 1
+    #H = np.multiply(H, np.divide(W.T.dot(V/var), W.T.dot(W.dot(H)/var)+ l1_reg_H+l2_reg_H * H ))
+    #W = np.multiply(W, np.divide((V/var).dot(H.T), (W.dot(H)/var).dot(H.T) + l1_reg_W + l2_reg_W * W ))
+    H = np.multiply(H, np.divide(W.T.dot(V), W.T.dot(W.dot(H))+ l1_reg_H+l2_reg_H * H ))
+    W = np.multiply(W, np.divide((V).dot(H.T), (W.dot(H)).dot(H.T) + l1_reg_W + l2_reg_W * W ))
 
-'''
-Functions for updating the W and H Matrices given the distribution,
-Each has a corresponding phi convex function, from this phi
-the functions psi= partial_derivative(phi) and zet= partial_derivative(psi) are found
-Then they applied in the following equations:
-
-wp = wp * ([(zet(WH) ⊙ V) H.T]p / [(zet(WH) ⊙ BC)  H.T]p )
-hp = hp * ([W.T (zet(WH) ⊙ V)]p / [W.T ⊙ (zet(WH)  BC)]p )
-'''
-
-
-
-def gaussian_phi(V,W,H):
-
-
-    H = np.multiply(H, np.divide(W.T.dot(V), W.T.dot(W.dot(H))))
-    W = np.multiply(W, np.divide(V.dot(H.T), (W.dot(H)).dot(H.T)))
 
     return W, H
 
-def poisson_phi(V,W,H):
+def poisson_phi(V,W,H,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H):
 
-
-    H = np.multiply(H, np.divide(W.T.dot(np.multiply(np.divide(1,W.dot(H)), V)), W.T.dot(np.multiply(np.divide(1,W.dot(H)), W.dot(H))) ) )
-    W = np.multiply(W, np.divide( np.multiply(np.divide(1,W.dot(H)), V).dot(H.T), np.multiply(np.divide(1,W.dot(H)), W.dot(H)).dot(H.T) ) )
-
-    return W, H
-
-def gamma_phi(V,W,H):
-
-    H = np.multiply(H, np.divide( W.T.dot(np.multiply(np.divide(1,(W.dot(H)))**2,V+1e-5)) , W.T.dot(np.multiply(np.divide(1,(W.dot(H))**2),W.dot(H)))) )
-    W = np.multiply(W, np.divide(np.multiply(np.divide(1,(W.dot(H))**2),V+1e-5).dot(H.T) ,  np.multiply(np.divide(1,(W.dot(H))**2), W.dot(H)).dot(H.T)))
+    zeta =  np.divide(1,W.dot(H))
+    H = np.mutliply(H, np.divide(W.T.dot(np.multiply(zeta, V)), W.T.dot(np.multiply(zeta, W.dot(H))) + l1_reg_H +l2_reg_H ) )
+    
+    zeta = np.divide(1,(W.dot(H)))**2
+    W = np.multiply(W, np.divide(np.multiply(zeta, V).dot(H.T), np.multiply(zeta, W.dot(H)).dot(H.T) + l1_reg_W+l2_reg_W) )
 
     return W, H
 
-def bernoulli_phi(V,W,H):
+def gamma_phi(V,W,H,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H):
+
+    zeta = np.divide(1,(W.dot(H)))**2
+    
+    H = np.multiply(H, np.divide(W.T.dot(np.multiply(zeta,V+1e-5)) , W.T.dot(np.multiply(zeta,W.dot(H))) + l1_reg_H+l2_reg_H)) 
+    
+    zeta = np.divide(1,(W.dot(H)))**2
+
+    W = np.multiply(W, np.divide(np.multiply(zeta,V+1e-5).dot(H.T) ,  np.multiply(zeta, W.dot(H)).dot(H.T)+ l1_reg_W+l2_reg_W))
+
+    return W, H
+
+def bernoulli_phi(V,W,H,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H):
 
     applied = binomial_apply_zeta(W,H,1)
-    H = np.multiply(H, np.divide( W.T.dot(np.multiply( applied, V )) , W.T.dot(np.multiply( applied, W.dot(H) )) ) )
+    H = np.multiply(H, np.divide( W.T.dot(np.multiply( applied, V )) , W.T.dot(np.multiply( applied, W.dot(H) )) + l1_reg_H+l2_reg_H) )
 
     applied = binomial_apply_zeta(W,H,1)
-    W = np.multiply(W, np.divide(np.multiply( applied, V ).dot(H.T), np.multiply( applied, W.dot(H) ).dot(H.T)  ) )
+    W = np.multiply(W, np.divide(np.multiply( applied, V ).dot(H.T), np.multiply( applied, W.dot(H) ).dot(H.T)+ l1_reg_W+l2_reg_W  ) )
 
 
     indices = np.where(W.dot(H)>1)
@@ -68,13 +65,13 @@ def bernoulli_phi(V,W,H):
 
 
 
-def binomial_phi(V,W,H,N):
+def binomial_phi(V,W,H,N,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H):
 
     applied = binomial_apply_zeta(W,H,N)
-    H = np.multiply(H, np.divide( W.T.dot(np.multiply( applied, V )) , W.T.dot(np.multiply( applied, W.dot(H) )) ) )
+    H = np.multiply(H, np.divide( W.T.dot(np.multiply( applied, V )) , W.T.dot(np.multiply( applied, W.dot(H) )) + l1_reg_H+l2_reg_H ) )
 
     applied = binomial_apply_zeta(W,H,N)
-    W = np.multiply(W, np.divide(np.multiply( applied, V ).dot(H.T), np.multiply( applied, W.dot(H) ).dot(H.T)  ) )
+    W = np.multiply(W, np.divide(np.multiply( applied, V ).dot(H.T), np.multiply( applied, W.dot(H) ).dot(H.T) + l1_reg_W+l2_reg_W ) )
 
     indices = np.where(W.dot(H)>N)
     count = 0
@@ -110,7 +107,7 @@ def cast_to_limit(W,H,limit):
 
     return product
 
-def multinomial_phi(V,W,H,N):
+def multinomial_phi(V,W,H,N,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H):
 
     applied = multinomail_apply_zeta(W,H,D)
     H = np.multiply(H, np.divide( W.T.dot(np.multiply( applied, V )) , W.T.dot(np.multiply( applied, W.dot(H) )) ) )
@@ -141,9 +138,10 @@ distribution_phi = {'gaussian' : gaussian_phi,
                     }
 
 class NMF:
-
-    def __init__(self,n_components=None, distribution = 'gaussian', error = 'frobenius', tol=1e-4, max_iterations = 200, random_state=None,
-      phi_update = False, N= None, D=None):
+    
+    
+    def __init__(self,n_components=None, distribution = 'gaussian', error = 'frobenius', tol=1e-4, max_iterations = 200, init=None,
+      phi_update = True, N= None, D=None, alpha=0.0, l1_ratio=0.0,random_state=None):
 
         ''' Constructor for this class. '''
         self.n_components = n_components
@@ -172,7 +170,10 @@ class NMF:
 
             raise ValueError('Give the Number of Dimensions')
         self.D = D
-
+        
+        self.alpha = alpha
+        self.l1_ratio = l1_ratio
+        self.init = init
 
 
     def printType(self):
@@ -189,24 +190,25 @@ class NMF:
 
         return W, H
 
-    def phi_update_rule(self,W, H):
+    def phi_update_rule(self,W,H,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H):
 
         #print('Phi Update')
         if self.V is None:
             raise ValueError('You should call the fit function first.')
 
         if self.distibution == 'multinomial':
-            W, H = distribution_phi[self.distibution](self.V, W, H, self.D)
+            W, H = distribution_phi[self.distibution](self.V, W, H, self.D,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H)
 
         elif self.distibution == 'binomial':
-            W, H = distribution_phi[self.distibution](self.V, W, H, self.N)
+            W, H = distribution_phi[self.distibution](self.V, W, H, self.N,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H)
 
         else:
-            W, H = distribution_phi[self.distibution](self.V, W, H)
+
+            W, H = distribution_phi[self.distibution](self.V, W, H,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H)
 
         return W, H
 
-    def fit(self, V):
+    def fit(self, V, W=None, H=None):
         #print('Fit the Model')
 
         self.V = V
@@ -230,25 +232,51 @@ class NMF:
             multiply = (self.N / self.n_components) ** 2
 
         # Find if a random State is given
-        if self.random_state is not None:
-            self.W = self.random_state.random_sample((n_samples,self.n_components)) * multiply
-            self.H = self.random_state.random_sample((self.n_components,n_features)) * multiply
+        if self.init is None:
+            if self.random_state is not None:
+            
+                self.W = self.random_state.random_sample((n_samples,self.n_components)) * multiply
+                self.H = self.random_state.random_sample((self.n_components,n_features)) * multiply
 
+            else:
+                self.W = np.random.random_sample((n_samples,n_features)) * multiply
+                self.H = np.random.random_sample((self.n_components,n_features)) * multiply
+        
+        elif self.init is 'custome':
+            
+            if W or H:
+                raise ValueError('For custome intitialization give the matrices W and H.')
+            self.W = W
+            self.H = H
+            
         else:
-            self.W = np.random.random_sample((n_samples,n_features)) * multiply
-            self.H = np.random.random_sample((self.n_components,n_features)) * multiply
+            
+            self.W, self.H = NMF_sk._initialize_nmf(V, n_components = self.n_components, init=self.init,
+                               random_state=self.random_state)
 
-    def transform(self, V):
+    def transform(self, V, max_iterations=None):
         #print('Transformation')
         if self.W is None or self.H is None:
             raise ValueError('You must first fit the NMF before tranforming the matrix.')
 
         W = self.W
         H = self.H
+        
+        if max_iterations:
+            self.max_iterations = max_iterations
 
         for i in range(0,self.max_iterations):
             if self.phi_update:
-                W, H = self.phi_update_rule(W,H)
+              
+                alpha_H = float(self.alpha)
+                alpha_W = float(self.alpha)
+                
+                l1_reg_W = alpha_W * self.l1_ratio
+                l1_reg_H = alpha_H * self.l1_ratio
+                l2_reg_W = alpha_W * (1. - self.l1_ratio)
+                l2_reg_H = alpha_H * (1. - self.l1_ratio)
+                
+                W, H = self.phi_update_rule(W,H,l1_reg_W,l1_reg_H,l2_reg_W,l2_reg_H)
 
             else:
                 W, H = self.update_rule(W,H)
@@ -265,3 +293,7 @@ class NMF:
         self.fit(V)
         W, H = self.transform(V)
         return W, H
+    
+    def get_components(self):
+        
+        return self.W , self.H
--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .MatrixFactorization.NMF import NMF
 from .MatrixFactorization.myNMF import myNMF
+from .MatrixFactorization import extmath.py
